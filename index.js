cad6a41ccc948c4858aa6456ad31399a2e8a8e60
Beyhan <beyhan.taylan@hotmail.com>
2022-04-30 17:00:43 -0400

    commit tree f700356eb25639241cfbc500f7d1fc15ed72cd46

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (num1, num2) => {
+    return num1 / num2
+}
+
+divide(2000,100);
+
+const square = (num3) => num3 ** num3
+
+const add = (num4, num5) => {
+    return num4 + num5
+}
